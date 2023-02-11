34683b4c8fcbd5d3bffc37440668a65d4fb0df65
Max Macaluso <maxwmacaluso@gmail.com>
2023-02-10 17:57:55 -0800

    Made OpenAI class and API Call

--- root/backend/README.md
+++ root/backend/README.md
@@ -4,9 +4,14 @@
     - Starts Node.js and Express.js Application Server at http://localhost:3001
 
 # Helpful Guides
-- https://vegibit.com/node-js-express-rest-api-tutorial/ (Node and Express)
-- https://rapidapi.com/blog/weather-app-javascript-nodejs/ (Node and external API)
-- https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786 (.env file)
+- Node:
+    - https://vegibit.com/node-js-express-rest-api-tutorial/ (Node and Express)
+    - https://rapidapi.com/blog/weather-app-javascript-nodejs/ (Node and external API)
+- .env File:
+    - https://www.twilio.com/blog/working-with-environment-variables-in-node-js-html (.env file)
+- Javascript Classes/OOP
+    - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes (JS OOP)
+    - https://javascript.info/class (Get/Set methods ES6)
 
 # General Tips
 - If getting errors after `npm start` try `npm install`
--- root/backend/apis/openai.js
+++ root/backend/apis/openai.js
@@ -1,25 +1,96 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 // So the program can find .env file
-require('dotenv').config();
+require('dotenv').config({path : '../.env'});
 
-const configuration = new Configuration({
-  apiKey: process.env.API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+class OpenAI {
+  // PRIVATE class attributes
+  #language;
+  #level;
+  #code;
+  #query;
+  #response;
 
-const testFunction = async () => {
-  const response = await openai.createCompletion({
-    model: "code-davinci-002",
-    prompt: "class Log:\n    def __init__(self, path):\n        dirname = os.path.dirname(path)\n        os.makedirs(dirname, exist_ok=True)\n        f = open(path, \"a+\")\n\n        # Check that the file is newline-terminated\n        size = os.path.getsize(path)\n        if size > 0:\n            f.seek(size - 1)\n            end = f.read(1)\n            if end != \"\\n\":\n                f.write(\"\\n\")\n        self.f = f\n        self.path = path\n\n    def log(self, event):\n        event[\"_event_id\"] = str(uuid.uuid4())\n        json.dump(event, self.f)\n        self.f.write(\"\\n\")\n\n    def state(self):\n        state = {\"complete\": set(), \"last\": None}\n        for line in open(self.path):\n            event = json.loads(line)\n            if event[\"type\"] == \"submit\" and event[\"success\"]:\n                state[\"complete\"].add(event[\"id\"])\n                state[\"last\"] = event\n        return state\n\n\"\"\"\nHere's what the above class is doing:\n1.",
-    temperature: 0,
-    max_tokens: 64,
-    top_p: 1.0,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-    stop: ["\"\"\""],
-  });
-  console.log(response.data)
-};
+  // CONSTRUCTOR
+  constructor(language, level, code) {
+    this.#language = language;
+    this.#level = level;
+    this.#code = code;
+    this.#query = '';
+    this.#response = '';
+  }
 
-testFunction();
+  // GETTERS
+  get language() {
+    return this.#language;
+  }
+  get level() {
+    return this.#level;
+  }
+  get code() {
+    return this.#code;
+  }
+  get query() {
+    return this.#query;
+  }
+  get response() {
+    return this.#response;
+  }
+
+  // SETTERS
+  set language(x) {
+    this.#language = x;
+  }
+  set level(x) {
+    this.#level = x;
+  }
+  set code(x) {
+    this.#code = x;
+  }
+  set query(x) {
+    this.#query = x;
+  }
+  set response(x) {
+    this.#response = x;
+  }
+
+  // METHODS
+  // TODO: query = code + level + language
+  formQuery() {
+    var x = "class Log:\n    def __init__(self, path):\n        dirname = os.path.dirname(path)\n        os.makedirs(dirname, exist_ok=True)\n        f = open(path, \"a+\")\n\n        # Check that the file is newline-terminated\n        size = os.path.getsize(path)\n        if size > 0:\n            f.seek(size - 1)\n            end = f.read(1)\n            if end != \"\\n\":\n                f.write(\"\\n\")\n        self.f = f\n        self.path = path\n\n    def log(self, event):\n        event[\"_event_id\"] = str(uuid.uuid4())\n        json.dump(event, self.f)\n        self.f.write(\"\\n\")\n\n    def state(self):\n        state = {\"complete\": set(), \"last\": None}\n        for line in open(self.path):\n            event = json.loads(line)\n            if event[\"type\"] == \"submit\" and event[\"success\"]:\n                state[\"complete\"].add(event[\"id\"])\n                state[\"last\"] = event\n        return state\n\n\"\"\"\nHere's what the above class is doing:\n1.";
+    // Invokes the setter
+    this.query = x;
+    return;
+  }
+  // Makes openAI API Call and stores `response.data` result in #response
+  async makeOpenAICall() {
+    const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY, });
+    const openai = new OpenAIApi(configuration);
+
+    // TODO: Move out of method
+    this.formQuery();
+
+    // TODO: cleanup
+    if (this.query.length < 0)
+    {
+      alert('Query not set!')
+    }
+
+    const response = await openai.createCompletion({
+      model: "code-davinci-002",
+      prompt: this.query,
+      temperature: 0,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["\"\"\""],
+    });
+
+    this.response = response.data;
+    console.log(this.response);
+  }
+}
+
+const test = new OpenAI('test', '12', '3');
+test.makeOpenAICall();
